c8ed881bbb20264f4b78309b81e968e00b147e77
cosmos510 <maxime.martin510.com>
2024-07-16 16:49:35 +0000

    add 8

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -5,7 +5,6 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const currentYear = getCurrentYear();
-
   const budget = {
     [`income-${currentYear}`]: income,
     [`gdp-${currentYear}`]: gdp,
