e728cfb73fec79aac4e2e8e0c6e4a5ab8c6ece84
cosmos510 <maxime.martin510.com>
2024-07-16 17:11:04 +0000

    add 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let i = 0; i < array.length; i++) {
-    array[i] = appendString + array[i];
+  const arr = [];
+  for (const idx of array) {
+    const val = appendString + idx;
+    arr.push(val);
   }
-  return array;
+
+  return arr;
 }
