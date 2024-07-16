24215a265ebba0673cd6f2224b6d987828c73cb4
cosmos510 <maxime.martin510.com>
2024-07-16 16:09:30 +0000

    style

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
-/*  */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const innertask = true;
+    const innertask2 = false;
   }
 
   return [task, task2];
