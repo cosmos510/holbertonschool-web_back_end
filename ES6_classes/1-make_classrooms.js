3215408120614b17f673781aa401968c3845846d
cosmos510 <maxime.martin510.com>
2024-07-17 08:37:38 +0000

    add 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -2,8 +2,8 @@ import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
   return [
-      new ClassRoom(19),
-      new ClassRoom(20),
-      new ClassRoom(34),
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
   ];
 }
