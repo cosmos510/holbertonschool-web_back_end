e3d4f556c8ad08fa51b742a0685dabec7963444f
cosmos510 <maxime.martin510.com>
2024-07-22 09:03:48 +0000

    love js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,4 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+  return [...set].filter((str) => str.startsWith(startString)).map((str) => str.slice(startString.length)).join('-');
+}
