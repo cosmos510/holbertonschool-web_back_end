e5df107f3ba727eb3483569f229a8451b9160b4e
cosmos510 <maxime.martin510.com>
2024-07-22 09:52:15 +0000

    style

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-export default function getResponseFromAPI(succes) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (succes) {
-      resolve({ status: 200, body: "Success" });
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
     } else {
-      reject(new Error("The fake API is not working currently"));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
