4f589ecea7197f689ed63fe70d0e9b3d5a452684
cosmos510 <maxime.martin510.com>
2024-07-22 08:33:36 +0000

    add 3

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+export default function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+  return students.map((student) => student.id).reduce((prev, next) => prev + next);
+}
