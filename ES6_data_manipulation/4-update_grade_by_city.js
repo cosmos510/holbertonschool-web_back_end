76d34620eb75340973f8ac0811559edebc01fbb7
cosmos510 <maxime.martin510.com>
2024-07-22 09:21:24 +0000

    eslint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,11 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
-      const grade = gradeObj ? gradeObj.grade : 'N/A';
-      return { ...student, grade };
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grade = newGrades.find((item) => item.studentId === student.id);
+      return {
+        ...student,
+        grade: grade ? grade.grade : 'N/A',
+      };
     });
 }
